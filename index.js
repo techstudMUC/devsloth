dbe05bc6798c1bfda4d0926286d437de415cdf1f
Lars Reisig <>
2018-12-09 02:50:32 +0100

    no message

---
+++ index.js
@@ -0,0 +1,99 @@
+
+
+const request = require('request');
+const express = require('express');
+const path = require('path');
+const fs=require("fs");
+var pictureUploaded = 0;
+var slothiness;
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'unsloth-dist', 'css')));
+app.use(express.static(path.join(__dirname, 'unsloth-dist', 'fonts')));
+app.use(express.static(path.join(__dirname, 'unsloth-dist', 'img')));
+app.use(express.static(path.join(__dirname, 'unsloth-dist', 'js')));
+app.use(express.static(path.join(__dirname, 'unsloth-dist')));
+
+app.listen(80, () => {
+    console.log("Server running on port 80");
+});
+
+// Replace <Subscription Key> with your valid subscription key.
+
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriBase = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false';
+
+// Request parameters.
+const params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'emotion'
+};
+
+
+
+
+app.post('/evaluateFace', function(req, res) {
+    const options = {
+        uri: uriBase,
+        qs: params,
+        body: req,
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key' : '177059b8a0e7408c9f8f291210915e6f'
+        }
+    };
+    request.post(options, (error, response, body) => {
+        if (error) {
+
+            console.log('Error: ', error);
+            return;
+        }
+        let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+        if (jsonResponse.includes("error")) {
+            pictureUploaded = -1;
+        }
+        else {
+            pictureUploaded = 1;
+        }
+        jsonObject = JSON.parse(body);
+        emotions = jsonObject[0].faceAttributes;
+        userData ={"slothiness" :{"text":"Potential", "className":"c-btn--warning"},
+            "Tasks": {"1":{"prioClass": "c-task--danger", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "01.01.2019"},
+            "2":{"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "02.01.2019"},
+            "3": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "03.01.2019"},
+            "4": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "04.01.2019"},
+            "5": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "05.01.2019"},
+            "6": {"prioClass": "c-task--success", "caption": "Monetisation (Share revenue with developers)", "type": "Übungsblatt", "date": "06.01.2019"}}};
+        console.log(JSON.stringify(emotions));
+        res.send();
+    });
+
+});
+
+
+app.get('/getUserData', function(req, res){
+
+res.json(userData);
+
+});
+
+app.get('/', function(req,res) {
+
+    if (pictureUploaded > 0) {
+        filePath = __dirname + '/unsloth-dist/interface.html';
+        res.sendFile(filePath);
+    }
+    else {
+        filePath = __dirname + '/unsloth-dist/start.html';
+        res.sendFile(filePath);
+    }
+
+});
+
+function evalutateEmotions(emotionJSON) {
+
+
+}
---
+++ unsloth-dist/css/test2.js
@@ -0,0 +1,87 @@
+{
+    "originalEvent"
+:
+    {
+        "isTrusted"
+    :
+        true
+    }
+,
+    "type"
+:
+    "click", "target"
+:
+    {
+    }
+,
+    "currentTarget"
+:
+    {
+    }
+,
+    "relatedTarget"
+:
+    null, "timeStamp"
+:
+    87377.60000000708, "jQuery331029232804691714676"
+:
+    true, "delegateTarget"
+:
+    {
+        "jQuery3310292328046917146761"
+    :
+        {
+            "events"
+        :
+            {
+                "click"
+            :
+                [{
+                    "type": "click",
+                    "origType": "click",
+                    "guid": 16,
+                    "selector": ".c-toggle__btn",
+                    "needsContext": false,
+                    "namespace": ""
+                }, {
+                    "type": "click",
+                    "origType": "click",
+                    "guid": 19,
+                    "selector": ".c-toggle__btn",
+                    "needsContext": false,
+                    "namespace": ""
+                }]
+            }
+        }
+    ,
+        "sizzle1544310962545"
+    :
+        {
+            "undefined"
+        :
+            {
+                "parentNode"
+            :
+                [4, 1, true]
+            }
+        }
+    }
+,
+    "handleObj"
+:
+    {
+        "type"
+    :
+        "click", "origType"
+    :
+        "click", "guid"
+    :
+        16, "selector"
+    :
+        ".c-toggle__btn", "needsContext"
+    :
+        false, "namespace"
+    :
+        ""
+    }
+}
